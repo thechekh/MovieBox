b1e002510e6643cc6b8de3405f2649e8c38e0920
ChekhDanil <chekhdan@gmail.com>
2019-10-25 12:24:37 +0300

    del app.css file(not usage)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import store from './store/index'
 import {Provider} from 'react-redux'
