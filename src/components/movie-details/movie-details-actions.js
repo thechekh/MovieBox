21d142ed37b434dbd79cdf6cc855cf5a9b7d250a
ChekhDanil <chekhdan@gmail.com>
2019-10-28 11:42:45 +0200

    add env variable to apikey

--- src/components/movie-details/movie-details-actions.js
+++ src/components/movie-details/movie-details-actions.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+const key = process.env.REACT_APP_API_KEY;
 export const getFilm = async (id) => {
-    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US`;
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
     let res = await axios.get(url);
     let {data} = res;
     return data;
--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-export const getFilms = async (page, url = `https://api.themoviedb.org/3/movie/now_playing?&language=en-US&api_key=ebea8cfca72fdff8d2624ad7bbf78e4c`) => {
+const key = process.env.REACT_APP_API_KEY;
+export const getFilms = async (page, url = `https://api.themoviedb.org/3/movie/now_playing?&language=en-US&api_key=${key}`) => {
     const fullUrl = `${url}&page=${page}`;
     let res = await axios.get(fullUrl);
     let {data} = res;
     return data;
 };
-export const getGenres = async (url = "https://api.themoviedb.org/3/genre/movie/list?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US") => {
+export const getGenres = async (url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`) => {
     let res = await axios.get(url);
     let {data} = res;
     return data;
