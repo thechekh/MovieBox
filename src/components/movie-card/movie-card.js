f8e0e40e6b02d7cfb408009f2e6ad735636c586f
ChekhDanil <chekhdan@gmail.com>
2019-11-04 14:57:32 +0200

    test git add hooks husky

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -1,50 +1,41 @@
-import React from 'react';
-import {connect} from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import moment from "moment";
 
-import './movie-card.css';
+import "./movie-card.css";
 import MovieCardImage from "./movie-card-image";
 
+const MovieCard = props => {
+  const { poster, id, title, rate, type } = props;
+  const year = moment(props.year, "YYYY/MM/DD").year();
+  const genres = props.genres.filter(item => type.includes(item.id));
+  const genresNames = genres.map(item => item.name);
+  const genresString = genresNames.join(", ");
 
-const MovieCard = (props) => {
-    const {poster, id, title, rate, type} = props;
-
-    const year = props.year.split('-')[0];
-    const genres = props.genres.filter((item) => {
-        if (type.includes(item.id)) {
-            return item
-        }
-        return false;
-    });
-    const genresNames = genres.slice(0, 3).map(item => item.name);
-    const genresString = genresNames.join(', ');
-    return (
-        <div className=' col-6 col-lg-3 d-flex flex-column justify-content-end align-items-center'>
-            <MovieCardImage path={poster} year={year} id={id}/>
-            <div className="movie__desc d-flex align-items-center justify-content-around">
-                <div className="movie__about">
-                    <h2 className="movie__name">{title}</h2>
-                    <span className="movie__type">
-                            {
-                                genresString
-                            }
-                        </span>
-                </div>
-                <div className="movie__rate">{rate}</div>
-            </div>
+  return (
+    <div className=" col-6 col-lg-3 d-flex flex-column justify-content-end align-items-center">
+      <MovieCardImage path={poster} year={year} id={id} />
+      <div className="movie__desc d-flex align-items-center justify-content-around">
+        <div className="movie__about">
+          <h2 className="movie__name">{title}</h2>
+          <span className="movie__type">{genresString}</span>
         </div>
-    )
+        <div className="movie__rate">{rate}</div>
+      </div>
+    </div>
+  );
 };
 MovieCard.propTypes = {
-    id: PropTypes.number,
-    title: PropTypes.string,
-    year: PropTypes.string,
-    poster: PropTypes.string,
-    rate: PropTypes.number,
+  id: PropTypes.number,
+  title: PropTypes.string,
+  year: PropTypes.string,
+  poster: PropTypes.string,
+  rate: PropTypes.number
 };
 let mapStateToProps = state => {
-    return {
-        genres: state.genres,
-    }
+  return {
+    genres: state.genres
+  };
 };
 export default connect(mapStateToProps)(MovieCard);
--- src/components/pages/now-playing-page/now-playing-page.js
+++ src/components/pages/now-playing-page/now-playing-page.js
@@ -1,57 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import './now-playing-page.css';
+import "./now-playing-page.css";
 import MovieGrid from "../../movie-grid/movie-grid";
-import {connect} from "react-redux";
-import {getFilms} from "./now-playing-page-actions"
+import { connect } from "react-redux";
+import { getFilms } from "./now-playing-page-actions";
 import Spinner from "../../spinner";
 import Pagination from "../../pagination";
-import AppHeader from '../../app-header'
-
+import AppHeader from "../../app-header";
 class NowPlayingPage extends React.Component {
-
-    componentDidMount() {
-        const {page} = this.props.match.params;
-        this.props.getFilms(page);
+  componentDidMount() {
+    const { page } = this.props.match.params;
+    this.props.getFilms(page);
+  }
+  changePage = e => {
+    let { selected } = e;
+    selected++;
+    if (selected) {
+      this.props.history.push(`/page/${selected}`);
+      this.props.getFilms(selected);
     }
-
-    changePage = e => {
-        let {selected} = e;
-        selected++;
-        if (selected) {
-            this.props.history.push(`/page/${selected}`);
-            this.props.getFilms(selected)
-        }
-    };
-
-    render() {
-        const {page} = this.props.match.params;
-        if (this.props.loading) {
-            return <Spinner/>
-        }
-        return (
-            <>
-                <AppHeader/>
-                <MovieGrid films={this.props.films}/>
-                <Pagination initialPage={page || 1}
-                            pageCount={this.props.pages}
-                            changePage={this.changePage}
-
-                />
-            </>
-
-        )
+  };
+  render() {
+    const { page } = this.props.match.params;
+    if (this.props.loading) {
+      return <Spinner />;
     }
+    return (
+      <>
+        <AppHeader />
+        <MovieGrid films={this.props.films} />
+        <Pagination
+          initialPage={page || 1}
+          pageCount={this.props.pages}
+          changePage={this.changePage}
+        />
+      </>
+    );
+  }
 }
 
-const
-    mapStateToProps = state => {
-        return {
-            films: state.films.results,
-            loading: state.films.loading,
-            pages: state.films.total_pages,
-        }
-    };
-export default connect(mapStateToProps, {getFilms})(NowPlayingPage);
-
-
+const mapStateToProps = state => {
+  return {
+    films: state.films.results,
+    loading: state.films.loading,
+    pages: state.films.total_pages
+  };
+};
+export default connect(
+  mapStateToProps,
+  { getFilms }
+)(NowPlayingPage);
